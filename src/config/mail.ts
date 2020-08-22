e8f9e2c0332e3d06e5d6ab83dbcf9576bda1a6ac
nicolas.ddm <nicolasdenckerdemarco@gmail.com>
2020-08-22 16:25:46 -0300

    add connection with cache using Redis and add Lists Services responses to cache

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'meu-email@cadastro.na.aws',
+      name: 'Nicolas cadastrado na AWS',
+    },
+  },
+} as IMailConfig;
