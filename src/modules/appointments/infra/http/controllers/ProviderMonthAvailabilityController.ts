ba40b4d4bda51161911525597f2b49daf8028ccb
nicolas.ddm <nicolasdenckerdemarco@gmail.com>
2020-09-01 08:33:30 -0300

    fix api to work better for front-end

--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,8 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { provider_id, month, year } = request.body;
+    const { provider_id } = request.params;
+    const { month, year } = request.query;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailabilityService,
@@ -13,8 +14,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
