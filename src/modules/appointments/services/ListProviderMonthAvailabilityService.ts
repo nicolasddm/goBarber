d1727e5fde931c2f5f616908d14fbe2f62b611f0
nicolas.ddm <nicolasdenckerdemarco@gmail.com>
2020-09-03 07:37:46 -0300

    fix show availability of the day of today as true

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -42,7 +42,7 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
-      const compareDate = new Date(year, month - 1, day);
+      const compareDate = new Date(year, month - 1, day + 1);
 
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
